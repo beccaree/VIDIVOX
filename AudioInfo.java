f094b7a9be1fd3a2f9087cccaaa9c4ba0e8cfeda
rlee291 <rlee291@aucklanduni.ac.nz>
2015-10-12 14:34:14 +1300

    no content for insturctions but GUI is done

--- AudioInfo.java
+++ AudioInfo.java
@@ -3,41 +3,37 @@ package info;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
-import javax.swing.SwingConstants;
-import javax.swing.JTextPane;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JTextArea;
 
-public class MergingInfo extends JFrame {
+public class AudioInfo extends JFrame {
 
 	private JPanel contentPane;
 
 	/**
 	 * Create the frame.
 	 */
-	public MergingInfo() {
+	public AudioInfo() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 		
 		JPanel title_panel = new JPanel();
 		contentPane.add(title_panel, BorderLayout.NORTH);
 		
-		JLabel lblTitle = new JLabel("How To Merge");
+		JLabel lblTitle = new JLabel("How To Manipulate Audio");
 		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		title_panel.add(lblTitle);
 		
@@ -50,39 +46,47 @@ public class MergingInfo extends JFrame {
 		bottom.add(buttons_panel, BorderLayout.NORTH);
 		buttons_panel.setBorder(new EmptyBorder(10, 10, 10, 10));
 		
-		final JLabel lblSubtitle = new JLabel("Merging audio at the beginning");
-		final JTextArea textArea = new JTextArea("Instructions plz thx");
+		final JLabel lblSubtitle = new JLabel("Speaking with festival");
+		final JTextArea textArea = new JTextArea("fest ex");
 		
-		JButton btnMergeAudioAt = new JButton("Merge Audio at Beginning");
-		btnMergeAudioAt.addActionListener(new ActionListener() {
+		JButton btnSkipb = new JButton("Speak");
+		btnSkipb.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				lblSubtitle.setText("Merging audio at the beginning");
-				// change explanation
+				lblSubtitle.setText("Speaking with festival");
+				//change explanation
 			}
 		});
-		buttons_panel.add(btnMergeAudioAt);
+		buttons_panel.add(btnSkipb);
 		
-		JButton btnMergeAudioHere = new JButton("Merge Audio here...");
+		JButton btnMergeAudioHere = new JButton("Stop");
 		btnMergeAudioHere.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				lblSubtitle.setText("Merging audio at any point");
+				lblSubtitle.setText("Stopping the festival voice");
 				//change explanation
 			}
 		});
 		buttons_panel.add(btnMergeAudioHere);
 		
+		JButton btnPlay = new JButton("Save As MP3");
+		btnPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblSubtitle.setText("Saving your text as an MP3 (audio) file");
+				//change explanation
+			}
+		});
+		buttons_panel.add(btnPlay);
+		
 		JPanel explain_panel = new JPanel();
 		explain_panel.setBorder(new EmptyBorder(10, 10, 10, 10));
 		bottom.add(explain_panel);
 		explain_panel.setLayout(new BorderLayout(0, 0));
-		
+
 		lblSubtitle.setFont(new Font("Tahoma", Font.BOLD, 11));
 		explain_panel.add(lblSubtitle, BorderLayout.NORTH);
 		
 		textArea.setLineWrap(true);
 		textArea.setWrapStyleWord(true);
 		textArea.setEditable(false);
-		explain_panel.add(textArea);
+		explain_panel.add(textArea, BorderLayout.CENTER);
 	}
-
 }
---
+++ VideoInfo.java
@@ -0,0 +1,127 @@
+package info;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+
+public class VideoInfo extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Create the frame.
+	 */
+	public VideoInfo() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel title_panel = new JPanel();
+		contentPane.add(title_panel, BorderLayout.NORTH);
+		
+		JLabel lblTitle = new JLabel("How To Video Control");
+		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		title_panel.add(lblTitle);
+		
+		JPanel bottom = new JPanel();
+		contentPane.add(bottom, BorderLayout.CENTER);
+		bottom.setLayout(new BorderLayout(0, 0));
+		
+		JPanel buttons_panel = new JPanel();
+		buttons_panel.setBackground(Color.DARK_GRAY);
+		bottom.add(buttons_panel, BorderLayout.NORTH);
+		buttons_panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		final JLabel lblSubtitle = new JLabel("Playing and pausing the video");
+		final JTextArea textArea = new JTextArea("This button allows you to play and pause the video, you can only merge at certain points when you pause the video");
+		
+		JButton btnSkipb = new JButton();
+		btnSkipb.setIcon(new ImageIcon(getClass().getResource("/buttons/skipb.png")));
+		btnSkipb.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				lblSubtitle.setText("Skipping back in intervals");
+				textArea.setText("This button allows you to skip backwards in the video, you can choose to set the interval at which it skips at by going to Edit>Set Skip Interval to...");
+			}
+		});
+		buttons_panel.add(btnSkipb);
+		
+		JButton btnRewind = new JButton();
+		btnRewind.setIcon(new ImageIcon(getClass().getResource("/buttons/rewind.png")));
+		btnRewind.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblSubtitle.setText("Rewinding the video");
+				//change explanation
+			}
+		});
+		buttons_panel.add(btnRewind);
+		
+		JButton btnPlay = new JButton();
+		btnPlay.setIcon(new ImageIcon(getClass().getResource("/buttons/play.png")));
+		btnPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblSubtitle.setText("Playing and pausing the video");
+				textArea.setText("This button allows you to play and pause the video, you can only merge at certain points when you pause the video");
+			}
+		});
+		buttons_panel.add(btnPlay);
+		
+		JButton btnForward = new JButton();
+		btnForward.setIcon(new ImageIcon(getClass().getResource("/buttons/forward.png")));
+		btnForward.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblSubtitle.setText("Fast Forwarding the video");
+				//change explanation
+			}
+		});
+		buttons_panel.add(btnForward);
+		
+		JButton btnSkipf = new JButton();
+		btnSkipf.setIcon(new ImageIcon(getClass().getResource("/buttons/skipf.png")));
+		btnSkipf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblSubtitle.setText("Skipping Forwards in intervals");
+				//change explanation
+			}
+		});
+		buttons_panel.add(btnSkipf);
+		
+		JButton btnSound = new JButton();
+		btnSound.setIcon(new ImageIcon(getClass().getResource("/buttons/unmute.png")));
+		btnSound.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblSubtitle.setText("Sound controls of the video");
+				//change explanation
+			}
+		});
+		buttons_panel.add(btnSound);
+		
+		JPanel explain_panel = new JPanel();
+		explain_panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		bottom.add(explain_panel);
+		explain_panel.setLayout(new BorderLayout(0, 0));
+		
+		lblSubtitle.setFont(new Font("Tahoma", Font.BOLD, 11));
+		explain_panel.add(lblSubtitle, BorderLayout.NORTH);
+		
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		textArea.setEditable(false);
+		explain_panel.add(textArea, BorderLayout.CENTER);
+	}
+}
